a53d04c5e5ecb90875b69caff6b118b12d46571e
Pi <piisthebestthingintheuniverse@gmail.com>
2024-04-07 13:59:23 -0700

    add more definitions

--- cursed_cpp.h
+++ cursed_cpp.h
@@ -4,6 +4,7 @@
 #define then ;
 #define counting int
 #define small_number uint16_t
+#define medium_number uint32_t
 #define yap std::string
 #define too_much std::vector
 #define spew std::cout <<
@@ -18,7 +19,11 @@
 #define goodbye return 0;
 #define gets_mogged_by <
 #define mogs >
+#define homies_with ==
+#define opps_with !=
 #define rizz ++
 #define fumble --
 #define yoink -
 #define yeet +
+#define tax %
+#define split_into /
