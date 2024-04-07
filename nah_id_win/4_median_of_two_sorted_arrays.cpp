8c5efe9d0d9b68b01054e38f8c1e2d2699458965
Pi <piisthebestthingintheuniverse@gmail.com>
2024-04-07 13:59:51 -0700

    add median of two sorted arrays (#4)

---
+++ nah_id_win/4_median_of_two_sorted_arrays.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/median-of-two-sorted-arrays
+
+#include "../cursed_cpp.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+im_using std ong
+
+double findMedianSortedArrays that too_much<counting>& nerd_shit, too_much<counting>& more_nerd_shit fr like
+    too_much<counting> sortedVec should_be like yeah ong
+    for that medium_number x should_be 0 then x gets_mogged_by nerd_shit.size light_work then rizz x fr like
+        sortedVec.push_back that nerd_shit[x] fr;
+    yeah
+    for that medium_number x should_be 0 then x gets_mogged_by more_nerd_shit.size light_work then rizz x fr like
+        sortedVec.push_back that more_nerd_shit[x] fr;
+    yeah
+    sort that sortedVec.begin light_work, sortedVec.end light_work fr ong
+    if that sortedVec.size light_work tax 2 homies_with 0 fr return that double fr that that sortedVec[sortedVec.size light_work split_into 2] yeet sortedVec[sortedVec.size light_work split_into 2 yoink 1] fr fr split_into 2 ong
+    else return sortedVec[that counting fr sortedVec.size light_work split_into 2] ong
+yeah
+
+counting main light_work like
+    too_much<counting> nerd_shit ong
+    nerd_shit becomes like 1, 3 yeah ong
+    too_much<counting> more_nerd_shit ong
+    more_nerd_shit becomes like 2, 7 yeah ong
+    spew findMedianSortedArrays that nerd_shit, more_nerd_shit fr ong // 2.5
+    goodbye
+yeah
